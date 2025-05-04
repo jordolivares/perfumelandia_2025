71cb49a4469e52ec027994da61251e957cfbaf01
jordolivares <jord.olivares@duocuc.cl>
2025-05-04 17:52:48 -0400

    Primer Avance

---
+++ src/main/java/com/example/perfumelandia/PerfumelandiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.perfumelandia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumelandiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumelandiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/perfumelandia/PerfumelandiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perfumelandia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumelandiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
