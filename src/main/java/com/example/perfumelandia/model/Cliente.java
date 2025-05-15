636bba0a99062bce324cf0d8b11c0e38bb0a924c
Jordan <jord.olivares@duocuc.cl>
2025-05-14 23:36:31 -0400

    Conexion Correcta con Base de Datos

--- src/main/java/com/example/perfumelandia/model/Cliente.java
+++ src/main/java/com/example/perfumelandia/model/Cliente.java
@@ -8,10 +8,28 @@ import lombok.NoArgsConstructor;
 import java.util.Date;
 
 @Entity
+@Table(name = "cliente")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class Cliente {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, length = 13, nullable = false)
+    private String run;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = false)
+    private String correo;
+
+
 }
