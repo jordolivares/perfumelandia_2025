297a2ec6de1614b513c1c0eb559f51b3d6bece35
Jordan <jord.olivares@duocuc.cl>
2025-06-25 21:00:23 -0400

    actualizacion

--- src/main/java/com/example/perfumelandia/controller/ControllerJordanInventario.java
+++ src/main/java/com/example/perfumelandia/controller/ControllerJordanInventario.java
@@ -3,6 +3,12 @@ package com.example.perfumelandia.controller;
 import com.example.perfumelandia.model.Inventario;
 import com.example.perfumelandia.service.InventarioService;
 import com.example.perfumelandia.service.ProductoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
+@Tag(name = "Inventario", description = "CRUD de inventario")
 public class ControllerJordanInventario {
 
 
@@ -22,14 +29,30 @@ public class ControllerJordanInventario {
     private InventarioService inventarioService;
 
 
+
     @PostMapping("/inventario")
+    @Operation(summary = "Crear Inventario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Inventario guardado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Inventario.class))),
+            @ApiResponse(responseCode = "404", description = "Inventario no guardado")
+    })
     public ResponseEntity<Inventario> crearInventario(@RequestBody Inventario inventario) {
 
         Inventario inventario1 = inventarioService.agregarInventario(inventario);
         return ResponseEntity.status(HttpStatus.CREATED).body(inventario1);
     }
 
+
     @GetMapping("/inventario")
+    @Operation(summary = "Buscar Todos Inventario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Inventario encontrado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Inventario.class))),
+            @ApiResponse(responseCode = "404", description = "Inventario no encontrado")
+    })
     public ResponseEntity<List<Inventario>> obtenerTodosInventario() {
 
         List<Inventario> inventario = inventarioService.buscarInventarios();
@@ -37,13 +60,21 @@ public class ControllerJordanInventario {
     }
 
     @GetMapping("/inventario/{id}")
-    public ResponseEntity<Inventario> obtenerInventario(@PathVariable Long id) {
+    public ResponseEntity<Inventario> obtenerInventarioId(@PathVariable Long id) {
 
         Inventario inventario = inventarioService.buscarInventarioPorId(id);
         return ResponseEntity.ok(inventario);
     }
 
+
     @PutMapping("/inventario/{id}")
+    @Operation(summary = "Actualizar Inventario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Inventario actualizado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Inventario.class))),
+            @ApiResponse(responseCode = "404", description = "Inventario no actualizado")
+    })
     public ResponseEntity<Inventario> actualizarInventario(@PathVariable Long id, @RequestBody Inventario inventario) {
         inventario.setId(id);
         inventarioService.agregarInventario(inventario);
@@ -51,6 +82,13 @@ public class ControllerJordanInventario {
     }
 
     @DeleteMapping("/inventario/{id}")
+    @Operation(summary = "Eliminar Inventario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Inventario eliminado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Inventario.class))),
+            @ApiResponse(responseCode = "404", description = "Inventario no elimando")
+    })
     public ResponseEntity<String> eliminarInventario(@PathVariable Long id) {
         inventarioService.eliminarInventario(id);
         return ResponseEntity.ok("Producto con ID " + id + " eliminado correctamente.");
--- src/main/java/com/example/perfumelandia/controller/ControllerJordanProducto.java
+++ src/main/java/com/example/perfumelandia/controller/ControllerJordanProducto.java
@@ -1,8 +1,15 @@
 package com.example.perfumelandia.controller;
 
 
+import com.example.perfumelandia.model.Inventario;
 import com.example.perfumelandia.model.Producto;
 import com.example.perfumelandia.service.ProductoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@Tag(name = "Producto", description = "CRUD de producto")
 public class ControllerJordanProducto {
 
 
@@ -19,12 +27,26 @@ public class ControllerJordanProducto {
     private ProductoService productoService;
 
     @PostMapping("/producto")
+    @Operation(summary = "Crear Producto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Producto creado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Inventario.class))),
+            @ApiResponse(responseCode = "404", description = "Producto no creado")
+    })
     public ResponseEntity<Producto> crearProducto(@RequestBody Producto producto) {
         productoService.guardarProducto(producto);
         return ResponseEntity.status(HttpStatus.CREATED).body(producto);
     }
 
     @GetMapping("/producto")
+    @Operation(summary = "Buscar Todos los Producto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Productos encontrado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Inventario.class))),
+            @ApiResponse(responseCode = "404", description = "Productos no encontrado")
+    })
     public ResponseEntity<List<Producto>> obtenerProductos() {
         List<Producto> productos = productoService.listarProductos();
         return ResponseEntity.ok(productos);
@@ -37,12 +59,27 @@ public class ControllerJordanProducto {
     }
 
     @PutMapping("/producto/{id}")
+    @Operation(summary = "Actualizar producto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Productos actualizado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Inventario.class))),
+            @ApiResponse(responseCode = "404", description = "Productos no actualizado")
+    })
     public ResponseEntity<Producto> actualizarProducto(@PathVariable Long id, @RequestBody Producto producto) {
          Producto nuevoProducto = productoService.actualizarProducto(id, producto);
         return ResponseEntity.ok(nuevoProducto);
     }
 
+
     @DeleteMapping("producto/{id}")
+    @Operation(summary = "Eliminar producto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Productos eliminado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Inventario.class))),
+            @ApiResponse(responseCode = "404", description = "Productos no eliminado")
+    })
     public ResponseEntity<String> eliminarProducto(@PathVariable Long id) {
         productoService.eliminarProducto(id);
         return ResponseEntity.ok("Producto eliminado correctamente.");
